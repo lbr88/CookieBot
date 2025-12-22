261fab03fde4ae8fa37da857df302f16b5c584bf
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-12-22 19:30:40 +0100

    Add versioning metadata for userscript auto-updates

--- CookieBot.user.js
+++ CookieBot.user.js
@@ -1,6 +1,13 @@
 // ==UserScript==
 // @name CookieBot
+// @namespace https://github.com/lbr88/CookieBot
+// @version 2.052
+// @description Automated bot for Cookie Clicker game
+// @author lbr88
 // @include /https?://orteil.dashnet.org/cookieclicker/
+// @updateURL https://github.com/lbr88/CookieBot/raw/development/CookieBot.user.js
+// @downloadURL https://github.com/lbr88/CookieBot/raw/development/CookieBot.user.js
+// @grant none
 // ==/UserScript==
 
 const readyCheck = setInterval(() => {
