946d3513525d76a4c36e006f136044ee49583a32
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-12-22 19:29:24 +0100

    Rewrite userscript to use Game.LoadMod() like CookieMonster

--- CookieBot.user.js
+++ CookieBot.user.js
@@ -1,47 +1,13 @@
 // ==UserScript==
-// @name         CookieBot - Auto Cookie Clicker
-// @namespace    https://github.com/prinzstani/CookieBot
-// @version      2.052
-// @description  Automated bot for Cookie Clicker game - handles purchasing, achievements, ascension, and more
-// @author       prinzstani, Lars Bo Rasmussen
-// @match        https://orteil.dashnet.org/cookieclicker/
-// @match        http://orteil.dashnet.org/cookieclicker/
-// @match        https://orteil.dashnet.org/cookieclicker/beta/
-// @match        http://orteil.dashnet.org/cookieclicker/beta/
-// @icon         https://orteil.dashnet.org/cookieclicker/img/favicon.ico
-// @grant        none
-// @run-at       document-end
+// @name CookieBot
+// @include /https?://orteil.dashnet.org/cookieclicker/
 // ==/UserScript==
 
-(function() {
-    'use strict';
+const readyCheck = setInterval(() => {
+ const Game = unsafeWindow.Game;
 
-    console.log('CookieBot userscript initializing...');
-
-    // Wait for Cookie Clicker to load
-    function initCookieBot() {
-        if (typeof Game === 'undefined' || !Game.ready) {
-            setTimeout(initCookieBot, 1000);
-            return;
-        }
-
-        console.log('Cookie Clicker loaded, injecting CookieBot...');
-
-        // Load CookieBot from GitHub
-        var script = document.createElement('script');
-        script.src = 'https://raw.githubusercontent.com/lbr88/CookieBot/development/cookieAutoPlayBeta.js';
-        script.onload = function() {
-            console.log('CookieBot loaded successfully!');
-            if (typeof AutoPlay !== 'undefined') {
-                console.log('CookieBot version:', AutoPlay.version);
-            }
-        };
-        script.onerror = function() {
-            console.error('Failed to load CookieBot from GitHub. Make sure the URL is correct.');
-        };
-        document.head.appendChild(script);
-    }
-
-    // Start initialization
-    initCookieBot();
-})();
+ if (typeof Game !== 'undefined' && typeof Game.ready !== 'undefined' && Game.ready) {
+  Game.LoadMod('https://raw.githubusercontent.com/lbr88/CookieBot/development/cookieAutoPlayBeta.js');
+  clearInterval(readyCheck);
+ }
+}, 1000);
