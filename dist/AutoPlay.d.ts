99ab41dacc54f2cbad7043680b1bc395c8341bae
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-12-23 17:04:24 +0100

    Bump version to 2.052.4

--- dist/AutoPlay.d.ts
+++ dist/AutoPlay.d.ts
@@ -3,7 +3,7 @@
  */
 import type { AutoPlayConfig, AutoPlayState } from './types/autoplay';
 export default class AutoPlay {
-    static readonly version = "2.052.3";
+    static readonly version = "2.052.4";
     private config;
     private state;
     private goldenCookieHandler;
--- dist/CookieBot.user.js
+++ dist/CookieBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name CookieBot (TypeScript)
 // @namespace https://github.com/lbr88/CookieBot
-// @version 2.052.3
+// @version 2.052.4
 // @description Automated bot for Cookie Clicker game (TypeScript version)
 // @author lbr88
 // @include /https?://orteil.dashnet.org/cookieclicker/
