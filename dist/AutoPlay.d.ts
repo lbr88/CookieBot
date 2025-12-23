786d6b76bc2646f1fcfe6a623dbff5df61f96cd5
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-12-23 17:21:13 +0100

    Bump version to 2.052.7

--- dist/AutoPlay.d.ts
+++ dist/AutoPlay.d.ts
@@ -3,7 +3,7 @@
  */
 import type { AutoPlayConfig, AutoPlayState } from './types/autoplay';
 export default class AutoPlay {
-    static readonly version = "2.052.6";
+    static readonly version = "2.052.7";
     private config;
     private state;
     private goldenCookieHandler;
