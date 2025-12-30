d608c7cbc8e162c2c2103eee0dcb97a69f695ef4
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-12-30 21:10:07 +0100

    .

--- src/AutoPlay.ts
+++ src/AutoPlay.ts
@@ -25,7 +25,7 @@ import { Logger } from './utils/Logger';
 
 export default class AutoPlay {
   // Version
-  static readonly version = '2.052-97';
+  static readonly version = '2.052-98';
 
   // State
   private config: AutoPlayConfig;
